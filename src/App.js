0ed05a915292ed2f35d166549ffb4a7ce3ac3172
Elex03 <eacuna042@gmail.com>
2024-09-19 13:01:42 -0600

    web: page in construction

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css'
+import './font.css'
+export const  App = () => {
+
+    return (
+        <div className="full-screen-div">
+            <div className='container-top'> 
+                <div className='title-team'>Team</div>
+                <div className='title-orangutan'>Orangutan</div>
+            </div>
+            <div className='container-bottom'>
+                <div className='text'>2024 Team Orangutan</div>
+                <div className='text text-center'> <span>Web in construction</span></div>
+                <div className='text'>Managua , Nicaragua</div>
+            </div>
+        </div>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import React from "react";
+import { App } from "./App";
 import ReactDOM from "react-dom/client";
 
 // Aplicacion inicial Elemento Root
 const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement);
 
-function Greeting() {
-
-    return <h1>Hello World {}</h1>;
-}
-
-root.render(Greeting());
+root.render(<App />);
